75a6ef7e699312697196d67db00191f901ee0bb2
7medo-77 <ahmedabdelhamid1997@gmail.com>
2025-05-24 21:52:25 +0300

    feat(components): Add post page with id dynamic route
    Slug could not be added to the url because the MOCKAPI free tier does
    not contain a slug endpoint. Component returns a client component
    PostDetail which contains edit and delete buttons

---
+++ src/app/(authorized)/posts/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { getPostById, Post } from "@/lib/api/post";
+import { notFound } from "next/navigation";
+import PostDetail from "../../../components/post-details/post.component";
+
+export default async function PostDetailPage({
+	params,
+}: {
+	params: { id: string };
+}) {
+	const { id } = params;
+	const response = await getPostById(undefined, id);
+
+	if (!response.success || !response.data) {
+		if (response.error?.includes("not authenticated")) {
+			return (
+				<div className="p-8 max-w-4xl mx-auto">
+					<div className="bg-red-50 border border-red-200 text-red-700 p-4 rounded-md">
+						You must be logged in to view this post.
+					</div>
+				</div>
+			);
+		}
+
+		return notFound();
+	}
+
+	const post = response.data as Post;
+	return <PostDetail post={post} />;
+}
